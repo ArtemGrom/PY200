d07c77a2d5ab7682cc25ae7d335f4e0d282f6f22
artgro <art.gromov@bk.ru>
2021-09-04 13:08:43 +0300

    init

---
+++ Основы ООП/Практические задания/task1_Glass__init__/main.py
@@ -0,0 +1,20 @@
+class Glass:
+    def __init__(self, capacity_volume: [int, float], occupied_volume: [int, float]):
+        if not isinstance(capacity_volume, (int, float)):
+            raise TypeError
+        if capacity_volume <= 0:
+            raise ValueError
+        self.capacity_volume = capacity_volume
+        self.occupied_volume = occupied_volume
+
+
+if __name__ == "__main__":
+    glass_1 = Glass(500, 0)
+    print(glass_1)
+    glass_2 = Glass(1000, 300)
+    print(glass_2)
+
+    glass_3 = Glass("500", 0)
+    # print(glass_3)
+    glass_4 = Glass(-500, 0)
+    # print(glass_4)
