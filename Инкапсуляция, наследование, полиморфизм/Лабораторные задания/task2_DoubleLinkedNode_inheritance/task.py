1b06915e93a4cd321a9d3f42b98d11af15835c72
artgro <art.gromov@bk.ru>
2021-09-25 13:27:01 +0300

    DoubleLinkedNode test 1.0

--- Инкапсуляция, наследование, полиморфизм/Лабораторные задания/task2_DoubleLinkedNode_inheritance/task.py
+++ Инкапсуляция, наследование, полиморфизм/Лабораторные задания/task2_DoubleLinkedNode_inheritance/task.py
@@ -5,53 +5,72 @@ class Node:
     """ Класс, который описывает узел связного списка. """
 
     def __init__(self, value: Any, next_: Optional["Node"] = None):
-        """
-        Создаем новый узел для односвязного списка
-        :param value: Любое значение, которое помещено в узел
-        :param next_: следующий узел, если он есть
-        """
+        """Инициализируем узел для двусвязного списка"""
         self.value = value
-        self.next = next_  # вызовется setter
-
-    def __repr__(self) -> str:
-        return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
-
-    def __str__(self) -> str:
-        return str(self.value)
-
-    @classmethod
-    def is_valid(cls, node: Any) -> None:
-        if not isinstance(node, (type(None), cls)):
-            raise TypeError
+        self.next = next_
 
     @property
     def next(self):
         return self._next
 
     @next.setter
-    def next(self, next_: Optional["Node"]):
+    def next(self, next_: Optional["Node"] = None):
         self.is_valid(next_)
         self._next = next_
 
+    @staticmethod
+    def is_valid(node: Any):
+        """Проверяем на верный формат входных данных"""
+        if not isinstance(node, (type(None), Node)):
+            raise TypeError("Неверный формат входных данных")
 
-# TODO реализовать класс DoubleLinkedNode
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def __repr__(self):
+        if self._next is None:
+            return f"Node({self.value}, {None})"
+        else:
+            return f"Node({self.value}, Node({self._next}))"
+
+
 class DoubleLinkedNode(Node):
-    """Класс, который описывает узел двусвязного списка"""
-    def __init__(self, value: Any, next_: Optional["Node"] = None, prev_: Optional["Node"] = None):
-        super().__init__(value, next_)
-        self.prev = prev_
+    """Класс, инициализирующий двусвязный список"""
 
-    def __repr__(self) -> str:
-        ...
-        # return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
+    def __init__(self, value: Any, next_: Optional["Node"] = None, prev: Optional["Node"] = None):
+        super().__init__(value, next_)
+        self.prev = prev
 
     @property
     def prev(self):
         return self._prev
 
     @prev.setter
-    def prev(self, prev_: Optional["Node"]):
-        self.is_valid(prev_)
-        self._prev = prev_
+    def prev(self, prev: Optional["Node"] = None):
+        self.is_valid(prev)
+        self._prev = prev
+
+    def __repr__(self):
+        if self._next is None and self._prev is None:
+            return f"Node({self.value}, {None}, {None})"
+        elif self._next is None:
+            return f"Node({self.value}, {None}, Node({self.prev})"
+        else:
+            return f"Node({self.value}, Node({self._next}), {None})"
 
 
+def test_double_linked_node():
+    first_node = DoubleLinkedNode(1)
+    second_node = DoubleLinkedNode(2)
+    third_node = DoubleLinkedNode(3)
+
+    first_node.next = second_node
+    third_node.prev = first_node
+    second_node.prev, second_node.next = first_node, third_node
+
+    assert repr(first_node) == "Node(1, Node(2), None)"
+    # print(repr(first_node))
+
+
+if __name__ == "__main__":
+    test_double_linked_node()
